c3104d49b7f63e836f126d9486c47cdabee0847b
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2023-12-29 14:44:17 -0300

    Novo commit

--- POKEDEX/assets/js/poke-api.js
+++ POKEDEX/assets/js/poke-api.js
@@ -28,10 +28,10 @@ PokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(PokeAPI.getPokemonDetail))
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(PokeAPI.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
 
