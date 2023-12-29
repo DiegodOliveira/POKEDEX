0e325b1d3b801ae83a2fe5ffc6c3a76e5c3ab836
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2023-12-29 10:46:14 -0300

    first commit

---
+++ POKEDEX/assets/js/main.js
@@ -0,0 +1,27 @@
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                </ol>
+
+                <img src=${pokemon.photo} 
+                alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+PokeAPI.getPokemons().then((pokemons = []) => {
+    const newHtml =  pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+    })
+    
